ba4b358a218e01ccb6a78b4c4acbc0ea0309bbc1
iwansafr <iwansafr@gmail.com>
2022-11-14 16:17:16 +0700

    final done

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,10 +7,12 @@ const Dashboard = () => {
   const [name, setName] = useState("")
   const [token, setToken] = useState("")
   const [expire, setExpire] = useState("")
+  const [users, setUsers] = useState([])
   const navigate = useNavigate()
 
   useEffect(() => {
     refreshToken()
+    getUsers()
   },[])
 
     const refreshToken = async() => {
@@ -51,13 +53,32 @@ const Dashboard = () => {
                 Authorization: `Bearer ${token}`
             }
         })
-        console.log(response.data)
+        setUsers(response.data)
     }
 
   return (
     <div className="container mt-5">
       <h1>Welcome Back: {name}</h1>
       <button className="button is-info" onClick={getUsers}>Get Users</button>
+      <table className="table is-striped is-fullwidth">
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            { users.map((user,index) => (
+                <tr key={user.id}>
+                    <td>{index+1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                </tr>
+            )) }
+            
+        </tbody>
+      </table>
     </div>
   );
 };
